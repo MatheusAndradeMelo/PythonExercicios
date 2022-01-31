ec7552797b3f384270b70add425b5b26fac0ea07
Matheus Andrade <93740980+MatheusAndradeMelo@users.noreply.github.com>
2022-01-31 20:05:09 -0300

    exercicios em python

---
+++ desafio03aula04.py
@@ -0,0 +1,5 @@
+print ('Vamos fazer a soma de dois numeros')
+numero1 = input('Informe o primeiro número:')
+numero2 = input('Informe um número para realizar a soma:')
+total = numero1 + numero2
+print ('O total da soma é =', total)
