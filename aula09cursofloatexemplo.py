71511306190b3134656ea0a221ed0912292a01ce
Matheus Andrade <93740980+MatheusAndradeMelo@users.noreply.github.com>
2022-01-31 21:12:15 -0300

    exercicios em python

---
+++ aula09cursofloatexemplo.py
@@ -0,0 +1,4 @@
+n1 = float ( input ('Informe um número real para a subtração:'))
+n2 = float (input ('Informe um número real para subtrair'))
+total = n1 - n2
+print ('O valor total de {} - {} é = {}'.format(n1, n2, total))
---
+++ aula09cursostrboolexemplo.py
@@ -0,0 +1,7 @@
+msg = str ( (input('Escreve alguma coisa ae cria : ')))
+print('Você digitou : {}'.format(msg))
+condicao = bool ( input('Digita ae só mais um bag p ver aqui uma parada : '))
+print (type(condicao))
+#Metodos com as váriaveis
+algumacoisa = input('Digita uma ultima coisa ae então rsrs : ')
+print(algumacoisa.isnumeric())
