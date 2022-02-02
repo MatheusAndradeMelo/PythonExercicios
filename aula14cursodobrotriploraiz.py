cfecfda92a3bb964a2338866a871b869c8044df7
Matheus Andrade <93740980+MatheusAndradeMelo@users.noreply.github.com>
2022-02-02 14:33:08 -0300

    exercicios em python

---
+++ aula14cursodobrotriploraiz.py
@@ -0,0 +1,5 @@
+n = int ( input('Digite um número : '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print ('O dobro do número {} é = {} \no triplo é = {} \ne a raiz é = {:.2f}'.format(n, d, t, r))
---
+++ aula18cursoconversaomoedas.py
@@ -0,0 +1,3 @@
+real = float (input('Informe quanto dinheiro você tem : R$'))
+dolar = real * 5.30
+print('Com R${:.2f} reais você comprar U$ {:.2f} dólares'.format(real, dolar))
